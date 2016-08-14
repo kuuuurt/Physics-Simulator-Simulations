4608515d81c482d7c2b42278b514307ac567f98b
Kurt Acosta <kurt.r.acosta@gmail.com>
2016-08-15 07:25:53 +0800

    Added Initial Concepts

--- Assets/Scripts/Centripetal and Centrifugal Forces/Models/CentripetalAndCentrifugalForcesModel.cs
+++ Assets/Scripts/Centripetal and Centrifugal Forces/Models/CentripetalAndCentrifugalForcesModel.cs
@@ -3,12 +3,5 @@ using System.Collections;
 
 public class CentripetalAndCentrifugalForcesModel : CentripetalAndCentrifugalForcesElement {
 
-	public float velocity;
-	public float time;
-	public float distance;
-
-	public float targetDistance;
-	public float startPos;
-	public float endPos;
 
 }
