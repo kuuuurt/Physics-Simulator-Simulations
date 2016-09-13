2af6fc749584a713ddbbd5886b8e0cbed2e812c6
Kurt Acosta <kurt.r.acosta@gmail.com>
2016-09-14 02:11:20 +0800

    Update Friction

---
+++ Assets/Scripts/Friction/Views/FrictionHUDView.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class FrictionHUDView : MonoBehaviour {
+
+	public Text forceAppliedText;
+	public Text coefficientOfFrictionText;
+	public Text netForceText;
+
+	public Slider forceApplied;
+}
