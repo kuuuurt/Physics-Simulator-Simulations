e993a264d2664ba2c919f92b762bb5f8d478dbca
Asdf Qwer <Asdf Qwer>
2016-09-21 15:55:28 +0800

    Added Models for Simulations

---
+++ Assets/Scripts/Centripetal and Centrifugal Forces/Views/CentripetalAndCentrifugalForcesHUDView.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CentripetalAndCentrifugalForcesHUDView : MonoBehaviour {
+	public Text massText;
+	public Text tangentialVelocityText;
+	public Text radiusText;
+	public Text centripetalForceText;
+	public Text centrifugalForceText;
+
+	public Slider mass;
+	public Slider tangentialVelocity;
+	public Slider radius;
+}
---
+++ Assets/Scripts/Uniform Circular Motion/Views/UniformCircularMotionHUDView.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UniformCircularMotionHUDView : MonoBehaviour {
+	public Text velocityText;
+	public Text radiusText;
+	public Text distanceText;
+	public Text timeText;
+
+	public Slider velocity;
+	public Slider radius;
+}
